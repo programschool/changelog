2ee820a8d0a73c8dfe2ff8c9265ce41aa30d0942
foxsir <838394225@qq.com>
2021-05-06 15:08:36 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+// .vuepress/config.js
+module.exports = {
+  head: [
+    ['link', { rel: 'icon', href: `http://programschool.com/assets/images/logo.svg` }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+  ],
+  themeConfig: {
+    search: false,
+    displayAllHeaders: true,
+    searchMaxSuggestions: 10,
+    logo: 'http://programschool.com/assets/images/logo.svg',
+    nav: [
+      { text: '编程学院', link: 'https://programschool.com/' }
+    ],
+    sidebar: [
+      ['/', '首页'],
+      ['/add-jupyter-mode', '增加阅读模式（jupyter模式）'],
+    ]
+  }
+}
---
+++ docs/add-jupyter-mode.md
@@ -0,0 +1,16 @@
+# 增加阅读模式（jupyter模式）
+#### *date: 2021年 5月 6日*
+
+# fsdfd
+
+fsdfd
+fsdf
+
+---
+
+#### 阅读模式可以更加**轻便**的学习教程，在阅读的同时可以在教程中运行编程语言基本代码
+
+![read-mode](/assets/read-mode.gif)
+
+
+jupyter 网站 https://jupyter.org/
